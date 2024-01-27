fe086098ba450e08378ad663210749ddc08d34cb
Aleksandras Petrovicevas <aleksandras.petrovicevas@nfq.lt>
2024-01-27 09:41:26 +0200

    Initial Commit

---
+++ ios-pokemon-app/ios_pokemon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_pokemon_appApp.swift
+//  ios-pokemon-app
+//
+//  Created by Aleksandras Petrovicevas on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct ios_pokemon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
